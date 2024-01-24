d0bb4dddcb9d7ffdafeadd4e02fece59fbf32643
boris <boriskozinn@gmail.com>
2024-01-24 17:05:09 +0300

    add gui for login

--- Ui/TracksSyncing.py
+++ Ui/TracksSyncing.py
@@ -6,12 +6,74 @@ import time
 import TracksComparator
 import SpLogin
 import Utils
+import customtkinter as ctk
+from PIL import Image
+import Locales
 
 
-class TracksSyncing:
-    def __init__(self):
+class TracksSyncing(ctk.CTkFrame):
+    def __init__(self, master, callback):
+        super().__init__(master)
+
         self._settings = SettingsStorage.Settings()
-        self._spotify_login = SpLogin.SpLogin()
+        self._locales = Locales.Locales()
+        self._callback = callback
+
+        self._title_frame = ctk.CTkFrame(self)
+
+        self._sync_image = ctk.CTkImage(Image.open(Utils.resource_path('icons/back.png')), size=(20, 20))
+        self._back_button = ctk.CTkButton(
+            self._title_frame,
+            image=self._sync_image,
+            command=lambda: callback(self),
+            text='',
+            width=20,
+            hover_color=("gray70", "gray30"),
+            fg_color="transparent",
+        )
+
+        self._sync_title = ctk.CTkLabel(
+            self._title_frame,
+            text=self._locales.get_string('sync_title'),
+            font=('Arial', 23, 'bold')
+        )
+
+        self._sync_frame = ctk.CTkFrame(self)
+        self._spotify_login = SpLogin.SpLogin(self._sync_frame, self._login_close_callback)
+
+        self._sync_frame.grid_columnconfigure((0, 1, 2), weight=1)
+        self._sync_frame.grid_rowconfigure((0, 1, 2), weight=1)
+
+        self._title_frame.grid_columnconfigure(0, weight=1)
+        self._title_frame.grid_rowconfigure(0, weight=1)
+
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self._sync_title.grid(row=0, column=0, sticky='ns')
+        self._back_button.grid(row=0, column=0, sticky='w', padx=2)
+        self._title_frame.grid(row=0, column=0, padx=5, pady=5, sticky='new', ipady=4)
+        self._sync_frame.grid(row=1, column=0, sticky='nsew', padx=5, pady=5)
+
+    def _login_close_callback(self):
+        self._spotify_login.grid_forget()
+
+        self._start_sync()
+
+    def initialize(self):
+        self._spotify_login.grid_forget()
+
+        if (res := self._spotify_login.spotify_login()) is None:
+            self._spotify_login.grid(row=1, column=1, sticky='nsew')
+
+        elif not res:
+            self._callback(self)
+
+        else:
+            self._start_sync()
+
+    def _start_sync(self):
+        pass
 
     def tracks_syncing(self):
         def print_menu():
